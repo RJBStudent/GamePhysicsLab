2aadcb8986501d7113248cd385bb6b8c8776a4f0
RJBStudent <richard.bourdelais@mymail.champlain.edu>
2019-09-08 18:18:18 -0400

    Added Project

---
+++ GamePhysicsUnity/GamePhysics/Assets/Scripts/Particle.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particle : MonoBehaviour
+{
+
+    public Vector2 position, velocity, acceleration;
+
+    public float rotation, angularVelocity, angularAcceleration;
+
+    //  step 2
+    void updatePositionExplicitEuler(float dt)
+    {
+        //  x(t+dt) = x(t) + v(t)dt
+        //  Euler:
+        //  F(t+dt) = F(t) + f(t)dt
+
+        position += velocity * dt;
+
+        // v(t+dt) = v(t) + a(t)dt
+        velocity += acceleration * dt;
+    }
+
+    void updatePositionKinematic(float dt)
+    {
+        position += velocity * dt + acceleration * dt * dt * .5f;
+
+        velocity += acceleration * dt;
+    }
+
+    void updateRotationEulerExplicit(float dt)
+    {
+        rotation += angularVelocity * dt;
+        angularVelocity += angularAcceleration * dt;
+    }
+    void updateRotationKinematic(float dt)
+    {
+        rotation += angularVelocity * dt + angularAcceleration * dt * dt * .5f;
+        angularVelocity += angularAcceleration * dt;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+    void FixedUpdate()
+    {
+        //  step 3
+        //  choose integrator
+        //updatePositionExplicitEuler(Time.fixedDeltaTime);
+        updatePositionKinematic(Time.fixedDeltaTime);
+
+        updateRotationEulerExplicit(Time.fixedDeltaTime);
+
+        // update transform
+        transform.position = position;
+        transform.rotation = Quaternion.Euler(0, 0, rotation);
+        //  step 4
+        //  test by faking motion along a curve
+        acceleration.x = -Mathf.Sin(Time.time);
+        angularAcceleration = Mathf.Sin(Time.time)*10f;
+    }
+}
---
+++ GamePhysicsUnity/GamePhysics/Assets/Scripts/Particle2D.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particle2D : MonoBehaviour
+{
+    //Step 1
+    public Vector2 position, velocity, acceleration;
+
+    public float rotation, angularVelocity, angularAcceleration;
+    
+    //Step 2
+    void updatePositionExplicitEuler(float dt)
+    {
+        // x(t+dt) = x(t) + v(t)dt
+        // Euler :
+        // F(t+dt) = F(t) + f(t)dt
+        //                + (dF/dt) dt
+        position += velocity * dt;
+
+        // v(t+dt) = v(t) + a(t)dt
+        // add dampling
+        velocity += acceleration *dt;
+
+    }
+
+    //Step 2
+    void updateRotationExplicitEuler(float dt)
+    {
+        // r(t+dt) = r(t) + av(t)dt
+        // Euler :
+        // F(t+dt) = F(t) + f(t)dt
+        //                + (dF/dt) dt
+        rotation += angularVelocity * dt;
+
+        // av(t+dt) = av(t) + aa(t)dt
+        // add dampling
+         angularVelocity += angularAcceleration * dt;
+
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // Step 3
+        // chose intergrator
+       // updatePositionExplicitEuler(Time.fixedDeltaTime);
+        updateRotationExplicitEuler(Time.fixedDeltaTime);
+
+        // update position
+        transform.position = position;
+        transform.rotation = Quaternion.Euler(0, 0, rotation);
+
+        // Step 4
+        // test by faking motion along a curve
+        acceleration.x = -Mathf.Sin(Time.time);
+        angularAcceleration = -Mathf.Sin(Time.time*0.01f);
+    }
+}
