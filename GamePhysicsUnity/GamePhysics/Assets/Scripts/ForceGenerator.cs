730fb25598bdd0373487ce801cb2173bd6dcaaa0
RJ Bourdelais <361rjb@gmail.com>
2019-09-09 10:19:53 -0400

    Started Lab 2 : Forces
    Added Mass, Gravity modifiyer, and adding force function

---
+++ GamePhysicsUnity/GamePhysics/Assets/Scripts/ForceGenerator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceGenerator
+{
+
+    public static Vector2 GenerateForce_Gravity(float particleMass, float gravitationalConstant, Vector2 worldUp)
+    {
+        //f = mg
+        Vector2 f_gravity = particleMass * gravitationalConstant * worldUp;
+        return f_gravity;
+    }
+}
--- GamePhysicsUnity/GamePhysics/Assets/Scripts/Particle.cs
+++ GamePhysicsUnity/GamePhysics/Assets/Scripts/Particle.cs
@@ -4,24 +4,66 @@ using UnityEngine;
 
 public class Particle : MonoBehaviour
 {
-
+    [Header("Transform Values")]
+    // lab 1 step 1
     public Vector2 position, velocity, acceleration;
 
+    //lab 1 step 1
     public float rotation, angularVelocity, angularAcceleration;
+    [Space]
+    [Header("Mass Values")]
 
-    //  step 2
+    //lab 2 step 1
+    public float startingMass = 1.0f;
+    float mass, massInv;
+
+    //lab 2 step 1
+    public void SetMass(float newMass)
+    {
+        //mass = (newMass > 0.0f) ? newMass : 0f;
+        mass = Mathf.Max(0, 0f, newMass);
+        massInv = mass > 0.0f ? 1.0f / mass : 0.0f;
+    }
+
+
+    //lab 2 step 1
+    public float GetMass()
+    {
+        return mass;
+    }
+
+    //lab 2 step 2
+    Vector2 force;
+    public void AddForce(Vector2 newForce)
+    {
+        //D'Alembert
+        force += newForce;
+    }
+
+    public void UpdateAcceleration()
+    {
+        //Newton 2
+        acceleration = force * massInv;
+
+        //REset because there are new forces coming next frame maybeee
+        force = Vector2.zero;
+    }
+
+    // lab 1  step 2
     void updatePositionExplicitEuler(float dt)
     {
         //  x(t+dt) = x(t) + v(t)dt
         //  Euler:
         //  F(t+dt) = F(t) + f(t)dt
 
+        
         position += velocity * dt;
 
         // v(t+dt) = v(t) + a(t)dt
         velocity += acceleration * dt;
     }
 
+    //lab 1 step 2
     void updatePositionKinematic(float dt)
     {
         position += velocity * dt + acceleration * dt * dt * .5f;
@@ -29,11 +71,14 @@ public class Particle : MonoBehaviour
         velocity += acceleration * dt;
     }
 
+    //lab 1 step 2
     void updateRotationEulerExplicit(float dt)
     {
         rotation += angularVelocity * dt;
         angularVelocity += angularAcceleration * dt;
     }
+
+    //lab 1 step 2
     void updateRotationKinematic(float dt)
     {
         rotation += angularVelocity * dt + angularAcceleration * dt * dt * .5f;
@@ -43,7 +88,7 @@ public class Particle : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        SetMass(startingMass);
     }
 
     // Update is called once per frame
@@ -59,15 +104,25 @@ public class Particle : MonoBehaviour
         //  choose integrator
         //updatePositionExplicitEuler(Time.fixedDeltaTime);
         updatePositionKinematic(Time.fixedDeltaTime);
+        
+        //Lab 2 
+        UpdateAcceleration();
 
+        //Lab 1
         updateRotationEulerExplicit(Time.fixedDeltaTime);
 
-        // update transform
+        // lab 1:  update transform
         transform.position = position;
         transform.rotation = Quaternion.Euler(0, 0, rotation);
-        //  step 4
+
+
+        // lab 1 step 4
         //  test by faking motion along a curve
-        acceleration.x = -Mathf.Sin(Time.time);
-        angularAcceleration = Mathf.Sin(Time.time)*10f;
+        //  acceleration.x = -Mathf.Sin(Time.time);
+        // angularAcceleration = Mathf.Sin(Time.time)*10f;
+
+        //Lab 2 test: apply gravity: f = mg
+
+        AddForce(ForceGenerator.GenerateForce_Gravity(mass, -9.81f, Vector2.up));
     }
 }
