bd8e77a3be41cd9b45a6e6b03dec9a57dbf7a699
RJ Bourdelais <361rjb@gmail.com>
2019-09-09 10:38:32 -0400

    Added function Definitions
    Step 3 definitions for lab 2, need to add implementation

--- GamePhysicsUnity/GamePhysics/Assets/Scripts/ForceGenerator.cs
+++ GamePhysicsUnity/GamePhysics/Assets/Scripts/ForceGenerator.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+//Lab 2 Force Generator step 3
 public class ForceGenerator
 {
 
@@ -10,5 +12,37 @@ public class ForceGenerator
         //f = mg
         Vector2 f_gravity = particleMass * gravitationalConstant * worldUp;
         return f_gravity;
+    }
+
+    public static Vector2 GenerateForce_normal(Vector2 f_gravity, Vector2 surfaceNormal_unit)
+    {
+        // f_sliding = f_gravity + f_normal
+
+        return surfaceNormal_unit;
+    }
+    
+    public static Vector2 GenerateForce_sliding(Vector2 f_gravity, Vector2 f_normal)
+    {
+        // f_friction_s = -f_opposing if less than max, else -coeff*f_normal
+    }
+
+    public static Vector2 GenerateForce_friction_static(Vector2 f_normal, Vector2 f_opposing, float frictionCoefficient_static)
+    {
+        // f_friction_k = -coeff*|f_normal| * unit(vel)
+    }
+
+    public static Vector2 GenerateForce_friction_kinetic(Vector2 f_normal, Vector2 particleVelocity, float frictionCoefficient_kinetic)
+    {
+    // f_drag = (p * u^2 * area * coeff)/2
+    }
+
+    public static Vector2 GenerateForce_drag(Vector2 particleVelocity, Vector2 fluidVelocity, float fluidDensity, float objectArea_crossSection, float objectDragCoefficient)
+    {
+        // f_spring = -coeff*(spring length - spring resting length)
+    }
+
+    public static Vector2 GenerateForce_spring(Vector2 particlePosition, Vector2 anchorPosition, float springRestingLength, float springStiffnessCoefficient)
+    {
+
     }
 }
