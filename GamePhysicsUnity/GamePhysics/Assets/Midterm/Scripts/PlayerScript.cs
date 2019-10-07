9b683aee31ff2b361e6a88843eef50bda8f07ad7
RJ <richard.bourdelais@mymail.champlain.edu>
2019-10-07 11:34:03 -0400

    Started Midterm Project

---
+++ GamePhysicsUnity/GamePhysics/Assets/Midterm/Scripts/PlayerScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+
+    [SerializeField, Range(1, 10)]
+    float xSpeed=1;
+    [SerializeField, Range(1, 10)]
+    float ySpeed=1;
+
+    Vector2 playerForce;
+
+    [SerializeField]
+    Particle thisParticle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerForce = new Vector2();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        GetInput();
+        thisParticle.AddForce(playerForce);
+    }
+
+    void GetInput()
+    {
+        playerForce.x = Input.GetAxis("Horizontal") * xSpeed;
+        playerForce.y = Input.GetAxis("Vertical") * ySpeed;
+    }
+}
