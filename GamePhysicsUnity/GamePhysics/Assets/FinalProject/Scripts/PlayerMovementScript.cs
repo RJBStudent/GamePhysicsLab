d3050c3885d8b794cefbb9c6c054546ea7ce5c7d
RJ <rj>
2019-12-05 10:45:19 -0500

    Added Particle Shooting

--- GamePhysicsUnity/GamePhysics/Assets/FinalProject/Scripts/PlayerMovementScript.cs
+++ GamePhysicsUnity/GamePhysics/Assets/FinalProject/Scripts/PlayerMovementScript.cs
@@ -19,10 +19,37 @@ public class PlayerMovementScript : MonoBehaviour
     float yInput = 0;
 
 
+
+    float shootInput = 0;
+    float lastShootInput = 0;
+    
+
+    [SerializeField]
+    int bulletCount;
+    [SerializeField]
+    GameObject playerBullet;
+    List<GameObject> bullets;
+    [SerializeField]
+    float projectileSpeed;
+
+    int currentBulletIndex = 0;
+
+    Vector3 direction;
+
+
     // Start is called before the first frame update
     void Start()
     {
         thisParticle = GetComponent<Particle3D>();
+
+        bullets = new List<GameObject>();
+        for (int i = 0; i < bulletCount; i++)
+        {
+            GameObject newBullet = Instantiate(playerBullet, transform.position, Quaternion.identity) as GameObject;
+            bullets.Add(newBullet);
+            newBullet.GetComponent<RotateAroundPlanetScrpt>().thePlanet = GetComponent<RotateAroundPlanetScrpt>().thePlanet;
+            newBullet.SetActive(false);
+        }
     }
 
     // Update is called once per frame
@@ -30,6 +57,7 @@ public class PlayerMovementScript : MonoBehaviour
     {
         UpdateInput();
         UpdatePosition();
+        ShootProjectile();
     }
 
     void UpdateInput()
@@ -37,20 +65,43 @@ public class PlayerMovementScript : MonoBehaviour
 
         xInput = Input.GetAxisRaw("Horizontal");
         yInput = Input.GetAxisRaw("Vertical");
-        
+
+        lastShootInput = shootInput;
+        shootInput = Input.GetAxisRaw("Shoot");
+       
 
     }
 
     Vector3 force = new Vector3();
     void UpdatePosition()
     {
-        force.x = xInput;
+        force.x = xInput * xSpeed;
         force.y = 0;
-        force.z = yInput;
+        force.z = yInput * ySpeed;
         
         thisParticle.AddLocalForce(force);
 
+        direction = transform.eulerAngles;
+        
+        
+        //transform.rotation = Quaternion.Euler(force);
+        
         //thisParticle.AddTorque(new Vector3(0, xInput * 100, 0))
        // thisParticle.angularVelocity = new Vector3(0, xInput * 100, 0);
+    }
+
+    void ShootProjectile()
+    {
+        if(shootInput == 1f && lastShootInput != 1f)
+        {
+            currentBulletIndex = (currentBulletIndex + 1) % bulletCount;
+            bullets[currentBulletIndex].SetActive(true);
+            // bullets[currentBulletIndex].GetComponent<Particle>().position = thisParticle.position;
+            bullets[currentBulletIndex].transform.position = thisParticle.position + (direction.normalized * 1.2f);
+            bullets[currentBulletIndex].GetComponent<Particle3D>().position = bullets[currentBulletIndex].transform.position;
+            bullets[currentBulletIndex].GetComponent<CollisionHull3D>().enabled = true;
+            bullets[currentBulletIndex].GetComponent<Particle3D>().AddForce(direction.normalized * projectileSpeed);
+           // bullets[currentBulletIndex].GetComponent<Particle3D>().velocity = Vector2.zero;
+        }
     }
 }
